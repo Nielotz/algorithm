66b0d52ce5057dd9f4ffa7bc31e5bbaccf7b8820
Nielotz <mati2806@poczta.fm>
2020-03-27 17:33:25 +0100

    Implement algorithms for perfect number finder

--- integral_algorithms/perfect_numbers.py
+++ integral_algorithms/perfect_numbers.py
@@ -1,2 +1,43 @@
- def is_number_perfect(number):
- 	 
+# There is no need to test higher than sqrt(number), because:
+#     number equal x * y == sqrt(number) * sqrt(number) 
+#     By checking all numbers in range <2, sqrt(number)>
+#     that check: is number / x == int(y) if so then we know,
+#     only that x multiplied by that y equals number.  
+#     Checking for x higher than sqrt(number), is like checking;
+#         is number / y == int(x) what is same as number / x == int(y)
+
+from math import sqrt
+
+
+# Without any lib.
+def is_number_perfect(number):
+    sum = 1
+    i = 2 
+
+    while i * i < number:  # Equal to i < sqrt(number) but without any lib  
+        if number % i == 0:
+            sum += i + number // i
+        i += 1
+
+    # If sqrt of number is int, then add sqrt of number to sum only once
+    if i * i == number:
+        sum += number
+    return sum == number
+
+
+# Faster but requires lib
+def is_number_perfect_better(number):
+    sum = 1
+    limit = int(sqrt(number))
+
+    for i in range(2, limit):
+        if number % i == 0:
+            sum += i + number // i
+
+    if number == limit * limit:
+        sum += limit
+
+    return sum == num**ber
+
+
+[print(i) for i in range(1, 10000000) if is_number_perfect_better(i)]
